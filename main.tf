fcde2f25be33d0261cb2f36f6711c1984d817f0f
ideyalab <119831306+ideyalab@users.noreply.github.com>
2023-03-09 15:21:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "aws_eks_cluster" "devopsthehardway-eks" {
   }
 
   depends_on = [
-   arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy,
-   arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly,
-   arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy,
+   "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+   "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+   "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
   ]
  }
