9f5869ba45a95f70246e5edb13e7d1573bc96554
ideyalab <119831306+ideyalab@users.noreply.github.com>
2023-03-09 14:59:01 +0530

    Update main.tf
    [skip ci]

--- main.tf
+++ main.tf
@@ -27,7 +27,7 @@ resource "aws_eks_cluster" "devopsthehardway-eks" {
  resource "aws_eks_node_group" "worker-node-group" {
   cluster_name  = aws_eks_cluster.devopsthehardway-eks.name
   node_group_name = "devopsthehardway-workernodes"
-  node_role_arn  = aws_iam_role.workernodes.arn
+  node_role_arn  = "arn:aws:iam::269390828189:role/eks_cluster"
   subnet_ids   = [ "subnet-02a4a455d98d344a0", "subnet-0c1da9fc8ca557fbc"]
   instance_types = ["t3.medium"]
 
